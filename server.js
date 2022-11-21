04c8e1772909a0bf0290da6575fc76fd2739dc6a
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-21 05:41:00 -0500

    Created intial file structure, prompts and DB schema/seeds

---
+++ server.js
@@ -0,0 +1,109 @@
+const inquirer = require("inquirer");
+require("console.table");
+require("dotenv").config();
+const mysql = require("mysql2");
+
+// Create DB Conection
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }
+);
+
+// Import helper files - validate inputs
+const validate = require('./helpers/validate');
+
+setTimeout(() => {promptUser();}, 1); // wrapped in setTimeout to prevent cTable getting overwritten by the prompt
+
+// Prompt User for Choices
+ promptUser = async ()  => {
+  inquirer.prompt([
+      {
+        name: 'choices',
+        type: 'list',
+        message: 'Please select an option:',
+        choices: [
+          'View All Employees',
+          'View All Roles',
+          'View All Departments',
+          'View All Employees By Department',
+          'View Department Budgets',
+          'Update Employee Role',
+          'Update Employee Manager',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Remove Employee',
+          'Remove Role',
+          'Remove Department',
+          'Exit'
+          ]
+      }
+    ])
+    .then((answers) => {
+      const {choices} = answers;
+
+      switch (choices) {
+        case 'View All Employees':
+          //read.viewAllEmployees();
+          db.query("SELECT employee.id as id, employee.first_name as 'First Name', employee.last_name as 'Last Name', role.title as 'Job Title', role.salary as 'Salary', department.department_name as 'Department', employee.manager_id as 'Manager ID' FROM employee, role, department GROUP BY employee.id ORDER BY employee.id", (err, results) => {
+            console.table("\nEmployees", results); // Display results in a table
+        });
+          break;
+          case 'View All Departments':
+            //read.viewAllDepartments();
+            break;
+          case 'View All Employees By Department':
+            //read.viewEmployeesByDepartment();
+            break;
+          case 'Add Employee':
+            //create.addEmployee();
+            // viewAllEmployee after update
+            break;
+          case 'Remove Employee':
+            //removeEmployee();
+            break;
+          case 'Remove Employee':
+            //removeEmployee();
+            break;
+          case 'Update Employee Role':
+            //update.updateEmployeeRole();
+            //viewAll
+            break;
+          case 'Update Employee Manager':
+            //updateEmployeeManager();
+            break;
+          case 'View All Roles':
+            //read.viewAllRoles();
+            break;
+          case 'Add Role':
+            //create.addRole();
+            // viewAllRoles after update
+            break;
+          case 'Remove Role':
+            //removeRole();
+            break;
+          case 'Add Department':
+            //create.addDepartment();
+            // viewAllDepartment after update
+            break;
+          case 'View Department Budgets':
+           // read.viewDepartmentBudget();
+            break;
+          case 'Remove Department':
+            //removeDepartment();
+            break;
+          case 'Remove Department':
+            //removeDepartment();
+            break;
+          case 'Exit':
+            //exit
+            break;
+          default:
+            break;
+      }
+  });
+};
